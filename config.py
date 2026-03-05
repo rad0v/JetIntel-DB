c9acaa578bcefc0fb5a6a76d4e8544fb8f4afe64
Nibedan Pati <nibedanpati@gmail.com>
2026-03-06 02:04:22 +0530

    cloudinary and bulk upload jet

--- config.py
+++ config.py
@@ -10,3 +10,8 @@ MONGO_URL = os.getenv("MONGO_URL")
 JWT_SECRET = os.getenv("JWT_SECRET", "jetintel-super-secret-key-change-in-production")
 JWT_ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 60 * 24  # 24 hours
+
+# Cloudinary
+CLOUDINARY_CLOUD_NAME = os.getenv("CLOUDINARY_CLOUD_NAME")
+CLOUDINARY_API_KEY = os.getenv("CLOUDINARY_API_KEY")
+CLOUDINARY_API_SECRET = os.getenv("CLOUDINARY_API_SECRET")
